39f16386869ac6d92e3486d9ab76e5ab5d2fb11b
KeithBertram <Keith.M.Bertram@gmail.com>
2025-01-28 17:13:15 -0500

    Add new dependencies: expo-sharing, expo-sqlite, and jobdb

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,109 +1,156 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, Button } from "react-native";
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+import { Collapsible } from "@/components/Collapsible";
+import { ExternalLink } from "@/components/ExternalLink";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { JobTrakrDB, JobData } from "jobdb";
+import { useEffect, useState } from "react";
 
 export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
-  );
+    const [db, setDb] = useState<JobTrakrDB | null>(null);
+
+    useEffect(() => {
+        async function initDb() {
+            console.info("Initializing DB...");
+            const myDb = new JobTrakrDB(1);
+            const status = await myDb.OpenDatabase();
+            if (status === "Success") {
+                const db = myDb.GetDb();
+                console.info("DB Initialized. Opening...");
+                setDb(myDb);
+                console.info("DB Initialized");
+            } else {
+                console.error(`Failed to initialize DB with status: ${status}`);
+            }
+        }
+
+        initDb();
+    }, []);
+
+    const doDeleteDB = async () => {
+        await db?.DeleteDatabase();
+    };
+
+    const doDeleteDBData = async () => {
+        let newId = { value: 3n };
+        const deleteStatus = await db?.GetJobDB().DeleteJob(newId.value);
+        console.info(`Deleted job with id ${newId.value}. Status = ${deleteStatus}`);
+    };
+
+    const doCreateJobs = async () => {
+        console.info("Creating jobs...");
+
+        let newId = { value: 0n };
+        const createStatus = await db?.GetJobDB().CreateJob(newId, {
+            _id: 0n,
+            Code: "Test Company",
+            Name: "Test Location",
+            JobTypeId: 1n,
+            CustomerId: 1n,
+            JobLocation: "9940 Blacksmith Way",
+            StartDate: new Date(),
+            PlannedFinish: new Date(),
+            BidPrice: 1000.0,
+            JobStatus: "Active",
+        });
+
+        console.info(`Created new job with id ${newId.value}. Status = ${createStatus}`);
+
+        const updateStatus = await db?.GetJobDB().UpdateJob({
+            _id: newId.value,
+            Code: "Updated Test Company",
+            Name: "Updated Test Location",
+            JobTypeId: 11n,
+            CustomerId: 11n,
+            JobLocation: "Updated 9940 Blacksmith Way",
+            StartDate: new Date(),
+            PlannedFinish: new Date(),
+            BidPrice: 2000.0,
+            JobStatus: "Active",
+        });
+
+        console.info(`Updated job with id ${newId.value}. Status = ${updateStatus}`);
+    };
+
+    const doCreateCategories = async () => {
+        console.info("Now query all the jobs:");
+        const jobs: JobData[] = [];
+        const queryStatus = await db?.GetJobDB().FetchAllJobs(jobs);
+        for (const job of jobs) {
+            console.info(`     Job: ${job._id} = ${job.Code} - ${job.Name}`);
+            let newCatId = { value: 0n };
+            const createStatus = await db?.GetCategoryDB().CreateCategory(newCatId, {
+                _id: 0n,
+                JobId: job._id,
+                Code: "Test Cat",
+                CategoryName: "Test Cat Name",
+                EstPrice: 1000.0,
+                CategoryStatus: "Active",
+            });
+            console.info(`Created new category with id ${newCatId.value}. Status = ${createStatus}`);
+        }
+    };
+
+    const exportDb = () => {
+        console.info("exporting db");
+
+        db?.CopyFileToDownloads();
+        console.info("Done exporting db");
+    };
+
+    return (
+        <ParallaxScrollView
+            headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+            headerImage={
+                <IconSymbol
+                    size={310}
+                    color="#808080"
+                    name="chevron.left.forwardslash.chevron.right"
+                    style={styles.headerImage}
+                />
+            }>
+            <ThemedView style={styles.titleContainer}>
+                <ThemedText type="title">Explore</ThemedText>
+                {db === null ? (
+                    <ThemedText>Loading...</ThemedText>
+                ) : (
+                    <ThemedView>
+                        <Button
+                            title="Create Jobs"
+                            onPress={doCreateJobs}
+                        />
+                        <Button
+                            title="Create Categories"
+                            onPress={doCreateCategories}
+                        />
+                        <Button
+                            title="Export Db"
+                            onPress={exportDb}
+                        />
+                        <Button
+                            title="Delete Db"
+                            onPress={doDeleteDB}
+                        />
+                    </ThemedView>
+                )}
+            </ThemedView>
+            <ThemedText>This app includes example code to help you get started.</ThemedText>
+        </ParallaxScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
+    headerImage: {
+        color: "#808080",
+        bottom: -90,
+        left: -35,
+        position: "absolute",
+    },
+    titleContainer: {
+        flexDirection: "row",
+        gap: 8,
+    },
 });
